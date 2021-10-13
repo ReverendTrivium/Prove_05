90081cdca7cb94d3e2d633f43f84b67bfbe87eb8
Derrick Eberlein <91962608+ReverendTrivium@users.noreply.github.com>
2021-10-13 18:55:56 -0400

    Initial commit

---
+++ app/src/main/java/com/example/prove05/DisplayMessageActivity.java
@@ -0,0 +1,59 @@
+package com.example.prove05;
+
+import static com.example.prove05.MainActivity.BOOK;
+import static com.example.prove05.MainActivity.CHAPTER;
+import static com.example.prove05.MainActivity.VERSE;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String scripture = intent.getStringExtra(BOOK)
+                    + " " + intent.getStringExtra(CHAPTER)
+                    + ":" + intent.getStringExtra(VERSE);
+
+            Log.d("Prove05", "Received intent with " + scripture);
+
+            TextView textView = (TextView) findViewById(R.id.scriptureText);
+            textView.setText(scripture);
+        }
+
+    }
+    public void saveData(View view) {
+
+        Intent intent = getIntent();
+
+        SharedPreferences sharedPref = this.getSharedPreferences("Scripture",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(BOOK, intent.getStringExtra(BOOK));
+        editor.putString(CHAPTER, intent.getStringExtra(CHAPTER));
+        editor.putString(VERSE, intent.getStringExtra(VERSE));
+        editor.commit();
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Scripture Saved", Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+
+    public void back (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/prove05/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.prove05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String BOOK ="com.example.prove05.BOOK";
+    public static final String CHAPTER = "com.example.prove05.CHAPTER";
+    public static final String VERSE = "com.example.prove05.VERSE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loadData(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Scripture Loaded", Toast.LENGTH_SHORT);
+        toast.show();
+        SharedPreferences sharedPref = this.getSharedPreferences("Scripture", Context.MODE_PRIVATE);
+        String book = sharedPref.getString(BOOK, "");
+        String chapter = sharedPref.getString(CHAPTER, "");
+        String verse = sharedPref.getString(VERSE, "");
+        EditText bookText = (EditText) findViewById(R.id.EditBook);
+        EditText chapterText = (EditText) findViewById(R.id.EditChapter);
+        EditText verseText = (EditText) findViewById(R.id.EditVerse);
+
+        bookText.setText(book);
+        chapterText.setText(chapter);
+        verseText.setText(verse);
+
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText bookText = (EditText) findViewById(R.id.EditBook);
+        EditText chapterText = (EditText) findViewById(R.id.EditChapter);
+        EditText verseText = (EditText) findViewById(R.id.EditVerse);
+
+
+        Log.d("","About to create intent with "
+                + bookText.getText().toString()
+                + " " + chapterText.getText().toString()
+                + ":" + verseText.getText().toString() );
+
+        intent.putExtra(BOOK, bookText.getText().toString());
+        intent.putExtra(CHAPTER, chapterText.getText().toString());
+        intent.putExtra(VERSE, verseText.getText().toString());
+        startActivity(intent);
+    }
+
+}
